afc4d49dd213153763d5968b7dd8b3b43b686e1e
kainthekiller <phillipyates12@hotmail.com>
2022-08-07 16:26:59 -0500

    Random Push dose this work ?

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,15 @@
 package HelloWorld;
 
 
+import javax.swing.plaf.synth.Region;
 
 public class HelloWorld {
 
     public static void main(String[] args)
     {
 
-
-    for (int test = 0; test <= 100; test++)
+        //<editor-fold desc="Here You need to make a for loop">
+        for (int test = 0; test <= 100; test++)
     {
         if(test % 2 == 0)
         {
@@ -19,9 +20,11 @@ public class HelloWorld {
         {
             System.out.println("ODD ?");
         }
+        System.out.printf("%d\n", test);
+    }
+        //</editor-fold>
 
     }
 
-    }
 
 }
---
+++ Person.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class Person {
+    enum apple { fighting, killing }
+
+   public  void appleSwitcher()
+   {
+       switch (apple)
+       {
+
+       }
+
+   }
+
+
+}
